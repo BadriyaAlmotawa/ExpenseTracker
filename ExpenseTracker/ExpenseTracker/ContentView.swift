b54ba5dbbd42e202175b2be2aefde40805d5a670
BadriyaAlmotawa <202003527@student.polytechnic.bh>
2026-06-30 12:56:22 +0300

    add expense database model and swiftdata schema

---
+++ ExpenseTracker/ExpenseTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTracker
+//
+//  Created by Macbook Pro on 30/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
